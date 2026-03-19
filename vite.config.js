51a07754a181cc6f22d201c3a2060fb810b60f52
Sulayman <keohavong.sirikone@gmail.com>
2026-03-19 07:23:54 +0100

    Features: routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path' // 👈 Ajout important pour gérer les chemins
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        // 👇 On déclare nos 3 pages officiellement ici !
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        superadmin: resolve(__dirname, 'superadmin.html')
+      }
+    }
+  }
 })
